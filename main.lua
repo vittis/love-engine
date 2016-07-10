5daded0a1e6a5fd31ae843a61090b6c9daf1f1e0
vittis <vitbolb1@gmail.com>
2016-07-10 18:37:44 -0300

    conf.lua adicionado

--- main.lua
+++ main.lua
@@ -1,50 +1,33 @@
-local utf8 = require("utf8")
+x=0
+y=0
 function love.load()
-	love.window.setTitle("Jogo Bom")
 	mainFont = love.graphics.newFont("KeepCalm.ttf", 60);
+	love.graphics.setFont(mainFont);
+
 	love.graphics.setBackgroundColor(255, 235, 205)
 	width, height = love.graphics.getDimensions()
-	love.keyboard.setKeyRepeat(true)
-	text = ""
 end
 
 function love.draw()
-	love.graphics.setFont(mainFont);
     love.graphics.setColor(0, 0, 0)
 	love.graphics.print("Jogo Bom", width/2-130, 0, 0)
 
-	love.graphics.print(text, 50, 200, 0, 1/2)
+    love.graphics.setColor(255, 0, 0)
+
+	love.graphics.line(love.graphics.getWidth()/2, love.graphics.getHeight()/2, x, y)
+
 end
 
 function love.update(dt)
-    fps = love.timer.getFPS();
-
+	x, y = love.mouse.getPosition()
 end
-
-function love.textinput(t)
-    text = text .. t
-end
-
 
 function love.keypressed(k)
 	if k == 'escape' then
 		love.event.quit()
 	end	
-    if k == 'backspace' then
-	    	if love.keyboard.hasTextInput() then
-	    	-- get the byte offset to the last UTF-8 character in the string.
-	        local byteoffset = utf8.offset(text, -1)
-	        if byteoffset then
-	            -- remove the last UTF-8 character.
-	            -- string.sub operates on bytes rather than UTF-8 characters, so we couldn't do string.sub(text, 1, -2).
-	            text = string.sub(text, 1, byteoffset - 1)
-	        end
-	    end
-    end
-    if k == 'tab' then
-    	love.keyboard.setTextInput(not love.keyboard.hasTextInput())
-    end
 end
+
 --[[ Funcao q confirma a saida com um popup
 function love.quit()
     opcaoSelecionada = love.window.showMessageBox("Atenção", "Realmente deseja sair?",
