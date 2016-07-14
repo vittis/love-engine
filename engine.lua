df11c936ac3a910387e5f2420302e2b871a7fc78
vittis <vitbolb1@gmail.com>
2016-07-14 17:10:19 -0300

    ajuste na engine

--- engine.lua
+++ engine.lua
@@ -11,13 +11,13 @@ end
 
 function engine:switch(stateFileName)
 	local finalStateFileName
-  local f=io.open(stateFileName..'.lua',"r")
-  if f~=nil then
-  	io.close(f)
-		finalStateFileName = stateFileName
-  else
+
+	if fileExists(stateFileName) then
+	 	finalStateFileName = stateFileName
+	else
 		finalStateFileName = 'Scenes/' .. stateFileName
 	end
+
 	self.previousStateName = engine.stateName
 	self.stateName = stateFileName
 	self.GS.switch(require(finalStateFileName))
@@ -25,13 +25,13 @@ end
 
 function engine:push(stateFileName)
 	local finalStateFileName
-	local f=io.open(stateFileName..'.lua',"r")
-	if f~=nil then
-		io.close(f)
-		finalStateFileName = stateFileName
+
+	if fileExists(stateFileName) then
+	 	finalStateFileName = stateFileName
 	else
 		finalStateFileName = 'Scenes/' .. stateFileName
 	end
+
 	self.previousStateName = engine.stateName
 	self.stateName = stateFileName
 	self.GS.push(require(finalStateFileName))
@@ -46,5 +46,13 @@ function engine:getCurrentState()
 	return self.GS.current()
 end
 
+local function fileExists(fileName)
+	local f=io.open(fileName..'.lua',"r")
+	if f~=nil then
+		io.close(f)
+		return true
+	end
+	return false
+end
 
 return engine
